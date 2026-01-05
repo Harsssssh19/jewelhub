14ca25e1869b218ccce68e82d4747f240f0cda9f
Dhrumil Chauhan <dhrumilc2005@gmail.com>
2026-01-05 16:39:59 +0530

    Run Project

---
+++ store/views.py
@@ -0,0 +1,267 @@
+import django
+import razorpay
+from django.contrib.auth.models import User
+from store.models import Address, Cart, Category, Order, Product
+from django.shortcuts import redirect, render, get_object_or_404
+from .forms import RegistrationForm, AddressForm
+from django.contrib import messages
+from django.views import View
+import decimal
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator # for Class Based Views
+
+
+# Create your views here.
+
+def home(request):
+    categories = Category.objects.filter(is_active=True, is_featured=True)[:3]
+    products = Product.objects.filter(is_active=True, is_featured=True)[:8]
+    context = {
+        'categories': categories,
+        'products': products,
+    }
+    return render(request, 'store/index.html', context)
+
+
+def detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    related_products = Product.objects.exclude(id=product.id).filter(is_active=True, category=product.category)
+    context = {
+        'product': product,
+        'related_products': related_products,
+
+    }
+    return render(request, 'store/detail.html', context)
+
+
+def all_categories(request):
+    categories = Category.objects.filter(is_active=True)
+    return render(request, 'store/categories.html', {'categories':categories})
+
+
+def category_products(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = Product.objects.filter(is_active=True, category=category)
+    categories = Category.objects.filter(is_active=True)
+    context = {
+        'category': category,
+        'products': products,
+        'categories': categories,
+    }
+    return render(request, 'store/category_products.html', context)
+
+
+# Authentication Starts Here
+
+class RegistrationView(View):
+    def get(self, request):
+        form = RegistrationForm()
+        return render(request, 'account/register.html', {'form': form})
+    
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, "Congratulations! Registration Successful!")
+            form.save()
+        return render(request, 'account/register.html', {'form': form})
+        
+
+@login_required
+def profile(request):
+    addresses = Address.objects.filter(user=request.user)
+    orders = Order.objects.filter(user=request.user)
+    return render(request, 'account/profile.html', {'addresses':addresses, 'orders':orders})
+
+
+@method_decorator(login_required, name='dispatch')
+class AddressView(View):
+    def get(self, request):
+        form = AddressForm()
+        return render(request, 'account/add_address.html', {'form': form})
+
+    def post(self, request):
+        form = AddressForm(request.POST)
+        if form.is_valid():
+            user=request.user
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            reg = Address(user=user, locality=locality, city=city, state=state)
+            reg.save()
+            messages.success(request, "New Address Added Successfully.")
+        return redirect('store:profile')
+
+
+@login_required
+def remove_address(request, id):
+    a = get_object_or_404(Address, user=request.user, id=id)
+    a.delete()
+    messages.success(request, "Address removed.")
+    return redirect('store:profile')
+
+@login_required
+def add_to_cart(request):
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    product = get_object_or_404(Product, id=product_id)
+
+    # Check whether the Product is alread in Cart or Not
+    item_already_in_cart = Cart.objects.filter(product=product_id, user=user)
+    if item_already_in_cart:
+        cp = get_object_or_404(Cart, product=product_id, user=user)
+        cp.quantity += 1
+        cp.save()
+    else:
+        Cart(user=user, product=product).save()
+    
+    return redirect('store:cart')
+
+
+@login_required
+def cart(request):
+    user = request.user
+    cart_products = Cart.objects.filter(user=user)
+
+    # Display Total on Cart Page
+    amount = decimal.Decimal(0)
+    shipping_amount = decimal.Decimal(10)
+    # using list comprehension to calculate total amount based on quantity and shipping
+    cp = [p for p in Cart.objects.all() if p.user==user]
+    if cp:
+        for p in cp:
+            temp_amount = (p.quantity * p.product.price)
+            amount += temp_amount
+
+    # Customer Addresses
+    addresses = Address.objects.filter(user=user)
+
+    context = {
+        'cart_products': cart_products,
+        'amount': amount,
+        'shipping_amount': shipping_amount,
+        'total_amount': amount + shipping_amount,
+        'addresses': addresses,
+    }
+    return render(request, 'store/cart.html', context)
+
+
+@login_required
+def remove_cart(request, cart_id):
+    if request.method == 'GET':
+        c = get_object_or_404(Cart, id=cart_id)
+        c.delete()
+        messages.success(request, "Product removed from Cart.")
+    return redirect('store:cart')
+
+
+@login_required
+def plus_cart(request, cart_id):
+    if request.method == 'GET':
+        cp = get_object_or_404(Cart, id=cart_id)
+        cp.quantity += 1
+        cp.save()
+    return redirect('store:cart')
+
+
+@login_required
+def minus_cart(request, cart_id):
+    if request.method == 'GET':
+        cp = get_object_or_404(Cart, id=cart_id)
+        # Remove the Product if the quantity is already 1
+        if cp.quantity == 1:
+            cp.delete()
+        else:
+            cp.quantity -= 1
+            cp.save()
+    return redirect('store:cart')
+
+
+@login_required
+def checkout(request):
+    user = request.user
+    address_id = request.GET.get('address')
+    
+    address = get_object_or_404(Address, id=address_id)
+    # Get all the products of User in Cart
+    cart = Cart.objects.filter(user=user)
+    for c in cart:
+        # Saving all the products from Cart to Order
+        Order(user=user, address=address, product=c.product, quantity=c.quantity).save()
+        # And Deleting from Cart
+        c.delete()
+    return redirect('store:orders')
+
+
+@login_required
+def orders(request):
+    all_orders = Order.objects.filter(user=request.user).order_by('-ordered_date')
+    return render(request, 'store/orders.html', {'orders': all_orders})
+
+
+def shop(request):
+    return render(request, 'store/shop.html')
+
+
+def test(request):
+    return render(request, 'store/test.html')
+
+def pay(request): 
+   error_message = None        
+   if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        modelselection = request.POST.get('modelselection')
+        amount = request.POST.get('amount')
+        message = request.POST.get('message')
+        client = razorpay.Client(
+                auth=('rzp_test_VQhEfe2NCXbbwI', '2ibreCYL78DA3kjOhobCvz0f'))
+
+        razorpay_payment = client.order.create(
+                dict(amount=(int(amount)*100), currency='INR'))
+
+        order_id = razorpay_payment['id']
+
+        booking= pay.objects.create(
+            name=name,
+            phone=phone,
+            email = email,
+            modelselection = modelselection,
+            amount = amount,
+            message =message,
+            order_id = order_id
+
+        )
+        booking.save()
+        razorpay_payment['name'] = name
+        razorpay_payment['amount']= amount 
+        razorpay_payment['order_id'] = order_id
+        form = pay(request.POST or None)
+        return render(request, 'book.html', {'razorpay_payment': razorpay_payment})
+   form = pay()
+   return render(request,'book.html', {'form': form, 'error_message':error_message})
+
+def success(request):
+    response = request.POST
+    params_dict = {
+        'razorpay_order_id': response['razorpay_order_id'],
+        'razorpay_payment_id': response['razorpay_payment_id'],
+        'razorpay_signature': response['razorpay_signature'],
+    }
+
+    client = razorpay.Client(
+        auth=('rzp_test_VQhEfe2NCXbbwI', '2ibreCYL78DA3kjOhobCvz0f'))
+
+    try:
+        status = client.utility.verify_payment_signature(params_dict)
+        razorpay_save = pay.objects.get(
+            order_id=response['razorpay_order_id'])
+        razorpay_save.razorpay_payment_id = response['razorpay_payment_id']
+        razorpay_save.paid = True
+        razorpay_save.save()
+        
+        
+    except Exception as e:
+        # Handle exceptions, for example, logging the error
+        print(f"Error occurred: {str(e)}")
+    return render(request, 'success.html', {'status': False})
